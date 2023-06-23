52cb285969c688dbfe227c76c7b8275480d0d8bd
JUTT-404 <137260765+JUTT-404@users.noreply.github.com>
2023-06-23 23:46:36 +0500

    utt.py

--- Jutt.py
+++ Jutt.py
@@ -1,9 +1,5 @@
 import os
 
-try:import httpx
-
-except:os.system('pip install httpx > /dev/null')
-
 try:import requests
 
 except:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
